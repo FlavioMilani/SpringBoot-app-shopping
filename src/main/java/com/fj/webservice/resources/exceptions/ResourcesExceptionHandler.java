a47894381594002b9fb6e126a222aac7cbdf08ec
Flavio Milani <frmilanij@gmail.com>
2023-07-27 16:06:43 -0300

    Exception handling - delete

--- src/main/java/com/fj/webservice/resources/exceptions/ResourcesExceptionHandler.java
+++ src/main/java/com/fj/webservice/resources/exceptions/ResourcesExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.fj.webservice.resources;
+package com.fj.webservice.resources.exceptions;
 
 import java.time.Instant;
 
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.fj.webservice.resources.exceptions.StandardError;
+import com.fj.webservice.services.exceptions.DatabaseException;
 import com.fj.webservice.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,6 +21,18 @@ public class ResourcesExceptionHandler {
 		
 		String error = "Recurso não encontrado";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), 
+				request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, 
+			HttpServletRequest request) {
+		
+		String error = "Erro no banco de dados.";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), 
 				request.getRequestURI());
 		
